26bcf1ffe03e19d089d65e2d5651a85104a4ac70
joseph danna <jodyanna14.15@gmail.com>
2020-10-29 10:39:06 -0500

    duplicate room check

--- client/src/components/RoomForm/index.js
+++ client/src/components/RoomForm/index.js
@@ -1,12 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, TextInput, SubmitButton } from './style';
 
 
 export default function RoomForm(props) {
   const [ text, setText ] = useState("");
+  const [ allRooms, setAllRooms ] = useState([]);
+
+  useEffect(() => {
+    const fetchRequest = async () => {
+      const response = await fetch('http://localhost:9000/rooms/all', {
+        method: 'GET',
+      }).catch(error => console.error('Error:', error));
+      if (response.ok) {
+        const data = await response.json();
+        return Promise.resolve(data)
+      } else return Promise.reject("Could not connect to API.")
+    };
+    fetchRequest().then(res => {
+      let allRooms = [];
+      for (let room of res.rooms) {
+        allRooms.push(room.name)
+      }
+      setAllRooms(allRooms)
+    })
+  }, []);
 
   const handleSubmit = event => {
     event.preventDefault();
+    if (allRooms.includes(text)) {
+      alert(`Room '${text}' already exists.`);
+      return
+    }
     fetch('http://localhost:9000/rooms/add', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
