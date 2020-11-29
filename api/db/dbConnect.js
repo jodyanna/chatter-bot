f433564c7872461d0549fcd41e8ec3d3f27abb6c
joseph danna <jodyanna14.15@gmail.com>
2020-11-29 12:07:34 -0600

    error handling for db connection

--- api/db/dbConnect.js
+++ api/db/dbConnect.js
@@ -1,6 +1,8 @@
 require('dotenv').config({path: __dirname + '../bin/.env'});
 const mysql = require('mysql2');
 
+if (process.env.DB_PORT == null) console.log("env variables are undefined.")
+
 const conn = mysql.createConnection({
   host     : process.env.DB_HOST,
   user     : process.env.DB_USER,
@@ -9,6 +11,11 @@ const conn = mysql.createConnection({
   port: process.env.DB_PORT,
   dateStrings: true,
   timezone : "+00:00"
+});
+
+conn.connect(err => {
+  if (err) throw err;
+  console.log("Connected to db successfully.")
 });
 
 module.exports = conn;
--- api/db/testConnection.js
+++
@@ -1,7 +0,0 @@
-const connection = require("./dbConnect");
-
-
-connection.connect(err => {
-  if (err) throw err;
-  console.log("Connection successful.")
-});
